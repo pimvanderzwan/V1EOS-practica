202bbc378fed7c7a1cc1ca32846067066965020b
Brian van der Bijl <peikos@peikos.net>
2019-02-21 11:28:24 +0100

    Unit 1 Opdracht 1 iets verhelderd met ToDo's

--- 1_os_level/shell.cc
+++ 1_os_level/shell.cc
@@ -2,29 +2,37 @@
 
 int main()
 { std::string input;
+
+  // ToDo: Vervang onderstaande regel: Laad prompt uit bestand
+  std::string prompt = "ToDo: Laad de prompt uit een bestand! ";
+
   while(true)
-  { std::getline(std::cin, input);
-    if (input == "new_file") new_file();
-    if (input == "ls") list();
-    if (input == "src") src();
-    if (input == "find") find();
-    if (input == "python") python();
-    if (input == "quit") return 0;
-    if (input == "error") return 1; } }
+  { std::cout << prompt;                   // Print het prompt
+    std::getline(std::cin, input);         // Lees een regel
+    if (input == "new_file") new_file();   // Kies de functie
+    else if (input == "ls") list();        //   op basis van
+    else if (input == "src") src();        //   de invoer
+    else if (input == "find") find();
+    else if (input == "python") python();
+    else if (input == "exit") return 0;
+    else if (input == "quit") return 0;
+    else if (input == "error") return 1;
 
-void new_file()
-{ std::cout << "NEW" << std::endl; }
+    if (std::cin.eof()) return 0; } }      // EOF is een exit
 
-void list()
-{ std::cout << "LS" << std::endl; }
+void new_file() // ToDo: Implementeer volgens specificatie.
+{ std::cout << "ToDo: Implementeer hier new_file()" << std::endl; }
 
-void find()
-{ std::cout << "FIND" << std::endl; }
+void list() // ToDo: Implementeer volgens specificatie.
+{ std::cout << "ToDo: Implementeer hier list()" << std::endl; }
 
-void python()
-{ std::cout << "PYTHON" << std::endl; }
+void find() // ToDo: Implementeer volgens specificatie.
+{ std::cout << "ToDo: Implementeer hier find()" << std::endl; }
 
-void src()
+void python() // ToDo: Implementeer volgens specificatie.
+{ std::cout << "ToDo: Implementeer hier python()" << std::endl; }
+
+void src() // Voorbeeld: Gebruikt SYS_open en SYS_read om de source van de shell (shell.cc) te printen.
 { int fd = syscall(SYS_open, "shell.cc", O_RDONLY, 0755); // Gebruik de SYS_open call om een bestand te openen.
   char byte[1];                                           // 0755 zorgt dat het bestand de juiste rechten krijgt (leesbaar is).
   while(syscall(SYS_read, fd, byte, 1))                   // Blijf SYS_read herhalen tot het bestand geheel gelezen is,
